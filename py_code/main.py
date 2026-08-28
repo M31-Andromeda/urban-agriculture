4702fe76385c7d0534ef7bc78cc96661b905f07d
M31-Andromeda <hugoandreu07@gmail.com>
2026-08-28 11:32:51 +0000

    sensor bugs solved2.0

--- py_code/main.py
+++ py_code/main.py
@@ -71,6 +71,14 @@ class SensorOrchestra:
             self.garden.amb_light, self.garden.ir = self.light_sensor.get_value()
             self.garden.voltage, self.garden.current = self.ina219.get_value()
             
+        ###------testing-------------
+        for atr, val in vars(self.garden).items():
+            if atr != "lock":
+                print(f"{atr}: {val}", end = " ")
+                
+        print()
+
+        ###------end-testing---------
         
         time.sleep(c.BEAT)
 
